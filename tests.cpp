1dcfbd78694a97881ad4dc8d208aa6703451c210
Kevin Young <kevinyoung18@gmail.com>
2020-03-15 21:02:59 -0700

    added more functions

--- tests.cpp
+++ tests.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <assert.h>
-#include <array>
 #include "aes128_constants.h"
+#include <array>
 
 using namespace std;
 
@@ -73,9 +73,9 @@ unsigned char* hexstr_to_uchar(string str)
 //@param: inputBytes: the input array of of 4 unsigned chars
 //@param: roundNum: the current round of transformation in the AES 
 //@return: the result of the g function on inputBytes as an array of 4 unsigned chars
-array<unsigned char,4> g(array<unsigned char, 4> inputBytes, int roundNum)
+array<unsigned char, 4> g(array<unsigned char, 4> inputBytes, int roundNum)
 {
-    array<unsigned char,4> outputBytes;
+    array<unsigned char, 4> outputBytes;
     outputBytes[0] = inputBytes[1];
     outputBytes[1] = inputBytes[2];
     outputBytes[2] = inputBytes[3];
@@ -88,32 +88,77 @@ array<unsigned char,4> g(array<unsigned char, 4> inputBytes, int roundNum)
 
     outputBytes[0] = outputBytes[0] ^ rcon[roundNum];
     return outputBytes;
-
 }
 
-
-unsigned char* get_round_key(string str)
+//TODO fix me 
+//performs a transform on a round key
+//@param: inputKey: the previous round key
+//@param: roundNum: the round number (0-11)
+//@return: an array of unsigned char corresponding to the round key
+array<unsigned char, 16> transform(array<unsigned char, 16> inputKey , int roundNum)
 {
-    unsigned char* key = hexstr_to_uchar(str);
-    cout << (int)key[0];
+    //pass the last 4 bytes through the g function and xor with the first 4 bytes
+    array<unsigned char, 4> lastBytes;
+    for(int i = 0; i < 4; i++)
+    {
+        lastBytes[i] = inputKey[i+11];
+    }
+    lastBytes = g(lastBytes, roundNum);
+    for(int i = 0; i < 4; i++)
+    {
+        inputKey[i] = inputKey[i] ^ lastBytes[i];
+    }
 
+    //xor bytes 4 - 15 with bytes 0-11 respectively
+    for(int i = 1; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            int index = (i*4) +j;
+            int lastIndex = (i-1) * 4 + j;
+            inputKey[index] = inputKey[index] ^ inputKey[lastIndex];
+        }
+    }
 }
 
+array<array<unsigned char, 16>,11> get_key_schedule(string str)
+{
+    unsigned char* currentKey = hexstr_to_uchar(str);
+    array<array<unsigned char, 16>,11> keySchedule;
+    
+    for(int i = 0; i < 11; i++)
+    {
+        unsigned char roundKey[16];
+        //transform
+    }
+
+}
+array<int, 2> getarr()
+{
+    array<int, 2> arr = {1,2};
+    return arr;
+}
 int main()
 {
+    //get_key_schedule("ff 09 a0 f0 f1 ff 01 10 ff 09 a0 f0 f1 ff 01 10");
+     array<unsigned char,4> in = {0x00, 0x01, 0x10, 0xFF};
+    array<unsigned char,4> o = g(in, 0);
+    for(int i = 0; i < 4; i++)
+    {
+        cout << (int)o[i] << " ";
+    }
 
-    string fileName;
-    string key;
-    ifstream input;
+    cout << endl;
 
-    cout << "Enter file name: " << endl;
-    cin >> fileName;
-    cout << "Enter encryption key: " << endl;
-    cin >> key;
-    
-    input.open(fileName);
-
-   
-    input.close();
+     array<unsigned char,16 > in1 = 
+    {0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0xFF, 0xFF, 0xFF, 0xFF,
+    0x00, 0x01, 0x10, 0xFF};
+    array<unsigned char,16> o1 = transform(in1, 0);
+    for(int i = 0; i < 16; i++)
+    {
+        cout << (int)o1[i] << " ";
+    }
     return 0;
 }
